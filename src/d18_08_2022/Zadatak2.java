b3f847a04aa2bd65db160cd8a969f09aafbb1ed0
Mirko Polic <mirko.polic@gmail.com>
2022-08-19 13:07:28 +0200

    Domaci 18.08.2022

--- src/d18_08_2022/Zadatak2.java
+++ src/d18_08_2022/Zadatak2.java
@@ -3,6 +3,24 @@ package d18_08_2022;
 public class Zadatak2 {
 
 	public static void main(String[] args) {
+//		.Kreirati klasu FacebookPost koja ima:
+//			   Od atributa:
+//			-	ime i prezime korisnika koji je objavio post
+//			-	ime i prezime korisnika na kom je profilu objavljen post (posto na fejsbuku mozete da postavite objavu na tudjem profilu)
+//			-	tekst objave
+//			-	broj lajkova
+//			-	broj deljenja
+//			  Od metoda:
+//			-	like(), koja povecava broj lajkova za 1.
+//			-	dislike(), koja smanjuje broj lajkova za 1 (broj lajkova ne moze da bude manji od nule)
+//			-	share(), koja povecava broj deljenja za 1
+//			-	print(), koja stampa objavu u formatu:
+//			(ime i prezime osobe koja je objavila) >>> (ime i prezime na cijem profilu)
+//			(tekst objave)
+//			Likes (broj lajkova) | Shares (broj deljenja)
+//
+//			U glavnoj klasi napraviti minimum dva FacebookPost-a i nad svakim pozvati svaku funkciju.
+
 
 		Footer footer = new Footer();//Stampa KRAJ PROGRAMA!
 
